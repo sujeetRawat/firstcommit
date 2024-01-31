afdca2758208b8a0a4acfd8d60e234a5206891dd
sujeetRawat <sujeetrawat890@gmail.com>
2024-01-31 18:35:46 +0530

    queryselector

---
+++ index.js
@@ -0,0 +1,25 @@
+
+const h1=document.querySelector('h1');
+h1.style.textAlign = "right";
+h1.style.padding="20px";
+
+const h2=document.querySelector('h2');
+h2.style.color="brown";
+
+const list=document.querySelector('.fruits');
+list.style.backgroundColor="grey";
+list.style.padding="20px";
+list.style.listStyleType="none";
+
+const fruits=document.querySelectorAll('.fruit');
+
+fruits[0].style.backgroundColor='white';
+fruits[0].style.padding="5px";
+fruits[1].style.backgroundColor='brown';
+fruits[1].style.color='white';
+fruits[1].style.padding="5px";
+fruits[2].style.backgroundColor='white';
+fruits[2].style.padding="5px";
+fruits[3].style.backgroundColor='brown';
+fruits[3].style.color='white';
+fruits[3].style.padding="5px";
